1a9ba56d64347043b92d11aaa1cbe3ed54b667e4
Your <you@example.com>
2025-04-29 19:20:59 -0300

    Implement VS sh

--- devsetup/scripts/editors/vs.sh
+++ devsetup/scripts/editors/vs.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-# Install editor
+# Verify
+version=$(vsixinstaller.exe --version)
+status=$?
 
-# Configure editor
+if [ $status -eq 0 ]; then
+    echo "vsixinstaller found version: $version"
+else
+    path=$(where vsixinstaller)
+    status=$?
+    if [ $status -eq 0 ]; then
+        echo "vsixinstaller found: $path"
+        echo "Trying to add path to use vsixinstaller command line."
+        # remove last part
+        path="${path%/*}"
+        echo "export PATH="'"$PATH':/$path'"' > ~/.bashrc
+    else
+        echo "Failed to find vsixinstaller, exit status: $status"
+        echo "Press enter to continue..."
+        read
+        exit 1
+    fi
+fi
+
 
 # Set up editor extensions
 vsixinstaller.exe /q /a /i:Microsoft.VisualStudio.Package.Roslyn
