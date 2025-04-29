6540d81c8169d5b697bc0fa4ffc83659e1154278
Your <you@example.com>
2025-04-29 17:17:17 -0300

    Implement init for git parameter and remove parameter from setup in git and editor

--- devsetup/cli/editors/editor_manager.py
+++ devsetup/cli/editors/editor_manager.py
@@ -2,12 +2,32 @@ from devsetup import typer
 from devsetup.cli.editors.registry_editor import get_editor
 from devsetup.globals import set_logger
 from devsetup.logger import Logger
+from devsetup.utils import print_error, print_msg
 
 app = typer.Typer()
 
 @app.command()
-def setup(name: str = typer.Option(..., "--name", "-n", help="Editor name in lowercase. (vscode, intellij, rider, vs ...)")):
+def setup():
     set_logger(Logger())
+
+    print_msg("Choose an editor:")
+    print_msg("1 - VS Code")
+    print_msg("2 - IntelliJ")
+    print_msg("3 - Rider")
+    print_msg("4 - Visual Studio")
+    ans = input("Your option: ")
+
+    if ans == "1":
+        name = "vscode"
+    elif ans == "2":
+        name = "intellij"
+    elif ans == "3":
+        name = "rider"
+    elif ans == "4":
+        name = "vs"
+    else:
+        print_error("Choose options typing the number 1, 2, ...")
+
     class_reference = get_editor(name)
 
     if class_reference == None:
--- devsetup/cli/git_repositories/git_repository_manager.py
+++ devsetup/cli/git_repositories/git_repository_manager.py
@@ -1,13 +1,37 @@
+from importlib import resources
 from devsetup import typer
 from devsetup.cli.git_repositories.registry_git_repository import get_git_repository
-from devsetup.globals import set_logger
+from devsetup.common import run_command
+from devsetup.globals import CONFIG, set_logger
 from devsetup.logger import Logger
+from devsetup.utils import print_error, print_msg
 
 app = typer.Typer()
 
 @app.command()
-def setup(name:str = typer.Option(..., "--name", "-n", help="Git Repository name in lowercase. (github, gitlab, ...)")):
+def init():
     set_logger(Logger())
+
+    with resources.path("devsetup.scripts", "git.sh") as p:
+        run_command(["bash", str(p), CONFIG["git"]["username"], CONFIG["git"]["email"]])
+
+
+@app.command()
+def setup():
+    set_logger(Logger())
+
+    print_msg("Choose a git repository:")
+    print_msg("1 - Gihub")
+    print_msg("2 - Gitlab")
+    ans = input("Your option: ")
+
+    if ans == "1":
+        name = "github"
+    elif ans == "2":
+        name = "gitlab"
+    else:
+        print_error("Choose options typing the number 1, 2, ...")
+
     class_reference = get_git_repository(name)
 
     if class_reference == None:
